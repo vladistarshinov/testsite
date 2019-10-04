6158864c48e8d5ba5cdfe686ede93468c47f4bc5
Vlad <vlad.s.starshinov@yandex.ru>
2019-10-04 17:38:39 +0300

    Добавили раздел "Угадайка"

---
+++ guess.js
@@ -0,0 +1,36 @@
+		var answer = parseInt(Math.random() * 100);
+		var tryCount = 0;
+		var maxTryCount = 3;
+
+		function readInt(){
+			var number = document.getElementById("userAnswer").value;
+			return parseInt(number);
+			// return +document.getElementById("userAnswer").value;
+		}
+
+		function write(text){
+			document.getElementById("info").innerHTML = text;
+		}
+
+		function hide(id){
+			document.getElementById(id).style.display = "none";
+		}
+
+		function guess(){
+			tryCount++;
+
+			var userAnswer = readInt();
+			if(userAnswer == answer){
+				write("<b>Поздравляю, вы угадали!</b>");
+				hide("button");
+				hide("userAnswer");
+			} else if(tryCount >= maxTryCount){
+				write("Вы проиграли<br>Правильный ответ: " + answer);
+				hide("button");
+				hide("userAnswer");
+			} else if(userAnswer > answer){
+				write("Вы ввели слишком большое число<br>Попробуйте еще раз. Введите число от 1 до 100");
+			} else if(userAnswer < answer){
+				write("Вы ввели слишком маленькое число<br>Попробуйте еще раз. Введите число от 1 до 100");				
+			}
+		}
